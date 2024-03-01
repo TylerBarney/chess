cf5aeab90feca9ca95679d48aa1cb205f9cd43c3
tylerbarney <willardbarney@gmail.com>
2024-03-01 15:12:13 -0700

    Got SQL database created

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,12 @@
 import chess.*;
+import dataAccess.DataAccessException;
+import dataAccess.MySqlDataAccess;
 import server.Server;
 
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, DataAccessException {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         new Server().run(8080);
         System.out.println("♕ 240 Chess Server: " + piece);
---
+++ server/src/main/java/dataAccess/MySqlDataAccess.java
@@ -0,0 +1,53 @@
+package dataAccess;
+
+import java.sql.SQLException;
+
+public class MySqlDataAccess {
+    public MySqlDataAccess() throws SQLException, DataAccessException {
+        configureDatabase();
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                `username` varchar(256) NOT NULL,
+                `password` varchar(256) NOT NULL,
+                `email` varchar(256) NOT NULL,
+                PRIMARY KEY (`username`),
+                INDEX(`username`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS auths (
+                `authToken` varchar(256) NOT NULL,
+                `username` varchar(256) NOT NULL,
+                PRIMARY KEY (`authToken`),
+                FOREIGN KEY (`username`) REFERENCES users(`username`),
+                INDEX(`authToken`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS games (
+                `gameID` int NOT NULL AUTO_INCREMENT,
+                `gameName` varchar(256) NOT NULL,
+                `whiteUsername` varchar(256) NULL,
+                `blackUsername` varchar(256) NULL,
+                `game` TEXT DEFAULT NULL,
+                PRIMARY KEY (`gameID`),
+                FOREIGN KEY (`whiteUsername`) REFERENCES users(`username`),
+                FOREIGN KEY (`blackUsername`) REFERENCES users(`username`),
+                INDEX(`gameID`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+    private void configureDatabase() throws DataAccessException, SQLException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statment : createStatements) {
+                try(var preparedStatement = conn.prepareStatement(statment)){
+                    preparedStatement.executeUpdate();
+                }
+            }
+        }
+    }
+}
